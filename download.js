f0ec49ea4387e36a447fcce121ac7280bd6ce898
tzuser <tzuser@yeah.net>
2018-07-03 00:51:00 +0800

    first commit

---
+++ download.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const path = require('path');
+const url=require('url');
+const request=require('request');
+
+function mkdirs(dirpath) {
+  if(!fs.existsSync(dirpath)){
+    if (!fs.existsSync(path.dirname(dirpath))) {
+      mkdirs(path.dirname(dirpath));
+    }
+    fs.mkdirSync(dirpath);
+  }
+}
+
+
+//从url里获取文件名及目录
+const getPathFromSrc=({src,userID,serverPath})=>{
+  let fileName,dir=`files/${userID}`;
+  const urlObj=url.parse(src);
+  let splitPathname=urlObj.pathname.split('/');
+  //视频处理
+  if(splitPathname.includes('video_file')){
+    //获取文件名称
+    fileName=splitPathname.pop();
+    if(!fileName.includes('.mp4'))fileName+=".mp4";
+
+  }else if(splitPathname.length>=3){//存在目录
+    //获取文件名称
+    fileName=splitPathname.pop();
+    //获取目录地址
+    dir+=splitPathname.join('/');
+  }else{
+    fileName=urlObj.pathname;
+  }
+  let fileSrc=path.join(dir,fileName);
+  dir=path.join(serverPath,dir);
+  return {fileName,dir,fileSrc}
+}
+
+
+//下载文件
+const download=async ({src,userID,serverPath})=>{
+  let {fileName,dir,fileSrc}=getPathFromSrc({src,userID,serverPath});
+  //创建目录
+  mkdirs(dir);
+  //下载文件路径
+  let filePath=path.join(dir,fileName);
+  //配置文件路径
+  let configFilePath=filePath+'.config';
+
+  //如果文件存在，配置文件不存在时，判定为已下载!
+  if(fs.existsSync(filePath) && !fs.existsSync(configFilePath)){
+    console.log(`The "${filePath}" already exists!`);
+    return fileSrc;
+  }
+  //写入配置文件
+  fs.writeFileSync(configFilePath,JSON.stringify({loadSize:0}))
+  //创建流
+  let fileStream=fs.createWriteStream(filePath);
+  //下载文件
+  await new Promise((resolve,reject)=>{
+    request({
+    proxy:{//代理
+      host:'127.0.0.1',
+      port:1080
+    },
+    method: 'GET',
+    uri:src,
+    }).on('end', resolve).on('error', reject).pipe(fileStream);
+  })
+
+  //下载完成删除配置文件
+  fs.unlinkSync(configFilePath)
+
+  return fileSrc;
+}
+module.exports=download;
---
+++ downloadDB.js
@@ -0,0 +1,53 @@
+const request=require('request');
+const rp=require('request-promise');
+const path=require('path');
+const headers=require('./headers');
+const download=require('./download');
+const sizeOf=require('image-size');
+const {postModel,userModel}=require('./db');
+const IMGHOST='https://78.media.tumblr.com';
+//服务路径
+const serverPath='G:\\work\\react-graphql';
+var options = {
+    proxy:{
+      host:'127.0.0.1',
+      port:1080
+    },
+    json: true
+};
+const getImgURL=(path)=>{
+  let startKey=path.indexOf('\\');
+  return `${IMGHOST}${path.substring(startKey)}`;
+}
+const main=async()=>{
+  let cursor;
+  cursor=postModel.find().populate('user').sort({_id:-1}).cursor()
+  let doc;
+  while(doc=await cursor.next()){
+    let {name:userID,avatar}=doc.user;
+
+    await download({src:getImgURL(avatar),userID,serverPath}).catch(err=>{
+        console.log(`avatar download error ${err}`)
+    })
+    
+    if(doc.thumbnail && doc.thumbnail.url){
+      //下载缩略图
+      let src=await download({src:getImgURL(doc.thumbnail.url),userID,serverPath}).catch(err=>{
+        throw `photo download error ${err}`
+      })
+      console.log(`thumbnail ${src} download completion`);
+    }
+    let promiseList=[]
+    for(var photo of doc.photos){
+
+      let src=download({src:getImgURL(photo.url),userID,serverPath}).catch(err=>{
+        throw `photo download error ${err}`
+      })
+      console.log(`photo download`);
+      promiseList.push(src)
+    }
+    await Promise.all(promiseList);
+    //下载图片列表
+  }
+}
+main()
